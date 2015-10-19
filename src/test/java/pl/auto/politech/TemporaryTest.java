9caf8dc151891d8771123b424702d2965fc027ef
kabubu <kabubu@o2.pl>
2015-10-19 15:11:46 +0200

    added TemporaryTest (pull request)

---
+++ src/test/java/pl/auto/politech/TemporaryTest.java
@@ -0,0 +1,14 @@
+package pl.auto.politech;
+
+import org.junit.Test;
+
+public class TemporaryTest extends AbstractApplicationTest{
+    @Override
+    public void setup() {
+
+    }
+    @Test
+    public void newTest(){
+
+    }
+}
