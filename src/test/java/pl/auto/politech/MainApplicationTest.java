3f37d8c875ae0641b616e3990692764b22aefa77
kabubu <kabubu@o2.pl>
2015-10-19 14:48:46 +0200

    cleaning

--- src/test/java/pl/auto/politech/MainApplicationTest.java
+++ src/test/java/pl/auto/politech/MainApplicationTest.java
@@ -4,9 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by VCM637 on 10/19/2015.
- */
+
 public class MainApplicationTest extends AbstractApplicationTest {
     @Override
     public void setup() {
