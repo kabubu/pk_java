ba9b79a9094fa1049c55efb328bad66685efbe58
wojot <wojotek@gmail.com>
2015-11-23 23:28:43 +0100

    fields visibility changed, wyswietlPojazd() method repaired

--- manufactory/src/main/java/pk/team/manufactory/VanCar.java
+++ manufactory/src/main/java/pk/team/manufactory/VanCar.java
@@ -1,57 +1,58 @@
 package pk.team.manufactory;
 
 public class VanCar extends Vehicle {
-	private int szerokoscPaki;
-	private int glebokoscPaki;
-	private int wysokoscPaki;
+    private int szerokoscPaki;
+    private int glebokoscPaki;
+    private int wysokoscPaki;
 
-	VanCar(TypNadwozia typNadowzia, int pojemnoscSilnika, int cena,
-			int pojemnoscBagaznika, int szerokoscPaki, int glebokoscPaki,
-			int wysokoscPaki, boolean dostepnyDlaKlientowEkskluzywnych) {
-		super(typNadowzia, pojemnoscSilnika, cena, pojemnoscBagaznika,
-				dostepnyDlaKlientowEkskluzywnych);
-		this.szerokoscPaki = szerokoscPaki;
-		this.glebokoscPaki = glebokoscPaki;
-		this.wysokoscPaki = wysokoscPaki;
-	}
+    VanCar(TypNadwozia typNadowzia, int pojemnoscSilnika, int cena,
+           int pojemnoscBagaznika, int szerokoscPaki, int glebokoscPaki,
+           int wysokoscPaki, boolean dostepnyDlaKlientowEkskluzywnych) {
+        super(typNadowzia, pojemnoscSilnika, cena, pojemnoscBagaznika,
+                dostepnyDlaKlientowEkskluzywnych);
+        this.szerokoscPaki = szerokoscPaki;
+        this.glebokoscPaki = glebokoscPaki;
+        this.wysokoscPaki = wysokoscPaki;
+    }
 
-	public int getSzerokoscPaki() {
-		return szerokoscPaki;
-	}
+    public int getSzerokoscPaki() {
+        return szerokoscPaki;
+    }
 
-	public void setSzerokoscPaki(int szerokoscPaki) {
-		this.szerokoscPaki = szerokoscPaki;
-	}
+    public void setSzerokoscPaki(int szerokoscPaki) {
+        this.szerokoscPaki = szerokoscPaki;
+    }
 
-	public int getGlebokoscPaki() {
-		return glebokoscPaki;
-	}
+    public int getGlebokoscPaki() {
+        return glebokoscPaki;
+    }
 
-	public void setGlebokoscPaki(int glebokoscPaki) {
-		this.glebokoscPaki = glebokoscPaki;
-	}
+    public void setGlebokoscPaki(int glebokoscPaki) {
+        this.glebokoscPaki = glebokoscPaki;
+    }
 
-	public int getWysokoscPaki() {
-		return wysokoscPaki;
-	}
+    public int getWysokoscPaki() {
+        return wysokoscPaki;
+    }
 
-	public void setWysokoscPaki(int wysokoscPaki) {
-		this.wysokoscPaki = wysokoscPaki;
-	}
-	
-	public String wyswietlPojazd() {
-		if (dostepnoscNaStanie) {
-			return "Id: " + this.id + ", dostepnosc na stanie: "
-					+ dostepnoscNaStanie + ", typ nadwozia: "
-					+ getTypNadwozia() + ", pojemnosc silnika: "
-					+ pojemnoscSilnika + ", pojemnosc bagaznika: "
-					+ pojemnoscBagaznika + ", cena: " + cena
-					+ ", dostepny dla klasy ekskluzywnej: "
-					+ dostepnyDlaKlientowEkskluzywnych
-					+ ", wymiary kontenera: " + szerokoscPaki + "x"
-					+ wysokoscPaki + "x" + glebokoscPaki;
-		}
-		return "nie ma takiego pojazdu";
-	}
+    public void setWysokoscPaki(int wysokoscPaki) {
+        this.wysokoscPaki = wysokoscPaki;
+    }
+
+    public String wyswietlPojazd() {
+        if (isDostepnoscNaStanie()) {
+            return "Id: " + getId() + ", dostepnosc na stanie: "
+                    + isDostepnoscNaStanie() + ", typ nadwozia: "
+                    + getTypNadwozia() + ", pojemnosc silnika: "
+                    + getPojemnoscSilnika() + ", pojemnosc bagaznika: "
+                    + getPojemnoscBagaznika() + ", cena: " + getCena()
+                    + ", dostepny dla klasy ekskluzywnej: "
+                    + isDostepnyDlaKlientowEkskluzywnych()
+                    + ", wymiary kontenera: " + szerokoscPaki + "x"
+                    + wysokoscPaki + "x" + glebokoscPaki;
+        }
+
+        return "nie ma takiego pojazdu";
+    }
 
 }
---
+++ manufactory/src/test/java/pk/team/manufactory/VanCarTest.java
@@ -0,0 +1,58 @@
+package pk.team.manufactory;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class VanCarTest {
+
+    private VanCar vanCar1;
+
+    @Before
+    public void setUp() throws Exception {
+    //vanCar1 = new VanCar(TypNadwozia.VAN, 3000, 40000, 0, 4, 10, 5, false);
+
+    }
+
+    @Test
+    public void testGetSzerokoscPaki() throws Exception {
+//        int pojemnoscSilnika = vanCar1.getSzerokoscPaki();
+//        assertEquals(4,pojemnoscSilnika);
+
+    }
+
+    @Test
+    public void testSetSzerokoscPaki() throws Exception {
+        System.out.println("VanCarTest.testSetSzerokoscPaki");
+    }
+
+    @Test
+    public void testGetGlebokoscPaki() throws Exception {
+
+    }
+
+    @Test
+    public void testSetGlebokoscPaki() throws Exception {
+
+    }
+
+    @Test
+    public void testGetWysokoscPaki() throws Exception {
+
+    }
+
+    @Test
+    public void testSetWysokoscPaki() throws Exception {
+
+    }
+
+    @Test
+    public void testWyswietlPojazd() throws Exception {
+        //String s = vanCar1.wyswietlPojazd();
+        //assertTrue(s.equals("tekst niezgodny"));
+       // assertEquals("tekst niezgodny",s);
+    }
+}
