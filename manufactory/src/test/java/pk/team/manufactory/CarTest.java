0f181928ee8ee87a2d0fd6beb522cccb3b49bed7
kabubu <kabubu@o2.pl>
2015-11-29 15:02:41 +0100

    added cartest input request

--- manufactory/src/test/java/pk/team/manufactory/CarTest.java
+++ manufactory/src/test/java/pk/team/manufactory/CarTest.java
@@ -1,11 +1,26 @@
 package pk.team.manufactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CarTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(System.in));
+        String line = "";
+        System.out.println("Podaj pojemnosc silnika:");
+        do {
+            line = in.readLine();
+        } while (!isCorrect(line));
+
+
+        System.out.println("You typed " + line);
 
 
         Vehicle standardCar1 = new StandardCar(TypNadwozia.SEDAN, 512312312, 10000, 400, false);
@@ -24,7 +39,6 @@ public class CarTest {
         vanCar2.wyswietlPojazd();
 
 
-
         List<Vehicle> autaOdWojtka = new ArrayList<>();
         autaOdWojtka.add(sportCar1);
         autaOdWojtka.add(standardCar1);
@@ -33,20 +47,27 @@ public class CarTest {
         autaOdWojtka.add(vanCar2);
 
 
-
-
         List<Vehicle> napewnoDobreAuta = new ArrayList<>();
-        for(Vehicle oneVehicle : autaOdWojtka){
-            if(oneVehicle.getPojemnoscSilnika() > 400 && oneVehicle.getPojemnoscSilnika() < 10000){
+        for (Vehicle oneVehicle : autaOdWojtka) {
+            if (oneVehicle.getPojemnoscSilnika() > 400 && oneVehicle.getPojemnoscSilnika() < 10000) {
                 napewnoDobreAuta.add(oneVehicle);
             }
         }
 
         System.out.println("------------------------------------");
-        napewnoDobreAuta.forEach(Vehicle::wyswietlPojazd) ;
+        napewnoDobreAuta.forEach(Vehicle::wyswietlPojazd);
 
 
+    }
 
+    private static boolean isCorrect(String line) {
+        int x= Integer.parseInt(line);
+        if( x > 400 && x < 10000){
+            return true;
+        } else{
+            System.out.println("Zla wartosc, wpisz ponownie");
+            return false;
+        }
     }
 }
 
