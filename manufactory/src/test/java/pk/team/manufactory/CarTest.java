94a3275215637d5781e1dd81f32b02ca10a0c245
wojot <wojotek@gmail.com>
2015-11-29 11:33:50 +0100

    CarTest repaired

--- manufactory/src/test/java/pk/team/manufactory/CarTest.java
+++ manufactory/src/test/java/pk/team/manufactory/CarTest.java
@@ -4,16 +4,21 @@ public class CarTest {
     private VanCar vanCar1;
 
     public static void main(String[] args) {
+
         Vehicle standardCar1 = new StandardCar(TypNadwozia.SEDAN, 1400, 10000, 400, false);
-        System.out.println(standardCar1.wyswietlPojazd());
+        standardCar1.wyswietlPojazd();
+
         Vehicle sportCar1 = new SportCar(TypNadwozia.SEDAN, 2500, 90000, 0, true, 300);
-        System.out.println(sportCar1.wyswietlPojazd());
+        sportCar1.wyswietlPojazd();
+
         Vehicle truck1 = new Truck(TypNadwozia.DOSTAWCZY, 3000, 40000, 0, false, 10, 5, 4, 2000);
-        System.out.println(truck1.wyswietlPojazd());
+        truck1.wyswietlPojazd();
+
         Vehicle vanCar1 = new VanCar(TypNadwozia.VAN, 3000, 40000, 0, 4, 10, 5, false);
-        System.out.println(vanCar1.wyswietlPojazd());
+        vanCar1.wyswietlPojazd();
+
         Vehicle vanCar2 = new VanCar(TypNadwozia.VAN, 3000, 40000, 0, 4, 10, 5, false);
-        System.out.println(vanCar2.wyswietlPojazd());
+        vanCar2.wyswietlPojazd();
     }
 }
 
